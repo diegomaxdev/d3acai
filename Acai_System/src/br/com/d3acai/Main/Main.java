77644070ff7117e37cf7151f657d3a7a21bb6461
diegomaxdev <46681411+diegomaxdev@users.noreply.github.com>
2019-08-18 16:08:37 -0300

    Subindo os arquivos base do meu projeto
    Iniciando meu projeto no Git

---
+++ Acai_System/src/br/com/d3acai/Main/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.d3acai.Main;
+
+/**
+ * @author Diego
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		System.out.println("Teste de Git");
+
+	}
+
+}
